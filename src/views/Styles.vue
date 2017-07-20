b9017ad3bae0950850ef783574a752f3f5382841
Leslie Doherty <leslie@catapultwebdevelopment.com>
2017-07-19 20:26:46 -0700

    Slowly getting rid of that old stuff I thought was ok.

--- src/views/Styles.vue
+++ src/views/Styles.vue
@@ -5,8 +5,6 @@
             <p>A little insight in to "How I Work". These are components rendered on the same page for easy reference.
                 Bonus! Quick UI check at breakpoints for the entire site in one place.</p>
         </header>
-        <v-layout row wrap>
-            <v-flex xs5>
                 <article id="typography">
 
                     <h1 id="headings">Headings</h1>
@@ -26,16 +24,82 @@
                     <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
 
                 </article>
-            </v-flex>
-            <v-flex xs5 offset-xs1>
+
 
                 <article id="forms">
 
 
 
                 </article>
-            </v-flex>
-        </v-layout>
+        <article id="grid">
+            <header>Default Grid Example</header>
+        <div class="columns">
+          <div class="column is-three-quarters">
+            <p class="notification is-info">
+              <code class="html">is-three-quarters</code>
+            </p>
+          </div>
+          <div class="column">
+            <p class="notification is-warning">Auto</p>
+          </div>
+          <div class="column">
+            <p class="notification is-danger">Auto</p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-two-thirds">
+            <p class="notification is-info">
+              <code class="html">is-two-thirds</code>
+            </p>
+          </div>
+          <div class="column">
+            <p class="notification is-warning">Auto</p>
+          </div>
+          <div class="column">
+            <p class="notification is-danger">Auto</p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-half">
+            <p class="notification is-info">
+              <code class="html">is-half</code>
+            </p>
+          </div>
+          <div class="column">
+            <p class="notification is-warning">Auto</p>
+          </div>
+          <div class="column">
+            <p class="notification is-danger">Auto</p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-one-third">
+            <p class="notification is-info">
+              <code class="html">is-one-third</code>
+            </p>
+          </div>
+          <div class="column">
+            <p class="notification is-success">Auto</p>
+          </div>
+          <div class="column">
+            <p class="notification is-warning">Auto</p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <p class="notification is-info">
+              <code class="html">is-one-quarter</code>
+            </p>
+          </div>
+          <div class="column">
+            <p class="notification is-success">Auto</p>
+          </div>
+        </div>
+        </article>
 
       <project-item>
 
