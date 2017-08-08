0fe1ec55515fa7677065fd7050d8551367539e61
Leslie Doherty <leslie@catapultwebdevelopment.com>
2017-08-08 16:29:34 -0700

    Small design style tweaks

--- src/components/project-item-description-list-item/ProjectItemDescriptionListItem.vue
+++ src/components/project-item-description-list-item/ProjectItemDescriptionListItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="project_item--description">
         <dt :id="'project_' + label + '_' + id">{{ label }} :</dt>
         <dd :aria-labelledby="'project_' + label + '_' + id">{{ content }}</dd>
     </div>
--- src/views/partials/landing/Landing.vue
+++ src/views/partials/landing/Landing.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="parent">
-    <header class="container" id="work">
+    <header class="container global_title--section-main" id="work">
       <h1 class="global_subtitle">Recent Work</h1>
     </header>
 
     <project-item v-bind:source="source" :items="items"></project-item>
     <section id="contact" class="contact contact-wrapper">
 
-      <header class="container">
+      <header class="container global_title--section-main">
         <h1 class="global_subtitle">Contact</h1>
       </header>
       <article class="container contact_form-wrapper">
