99949742b9802356f322ed1bf5d6fc302938f929
Leslie Doherty <ldoherty@tableau.com>
2020-01-10 20:27:12 +0000

    Updating for a11y posts and a few style changes for easier link contrast. Holding my breath and remembering this is an agile project. Here goes.

--- src/components/hero/Hero.vue
+++ src/components/hero/Hero.vue
@@ -2,8 +2,7 @@
   <section role="banner" class="hero header" :class="heroClass">
     <div class="hero-body">
       <div class="highlight">
-        <span>A passionate speaker and empathetic developer looking to work with great teams, produce beautiful code, and share insights along the way.
-          You can see a bit of how I work <router-link to="/styleguide">at the living style guide,</router-link> or see <a href="https://github.com/lesliedoherty/portfolio">the repo here</a>.
+        <span>A passionate speaker and empathetic developer on a mission to empower humans through technology, build connections, learn through collaboration, and lead through humility and compassion.
         </span>
       </div>
     </div>
--- src/views/partials/landing/Landing.vue
+++ src/views/partials/landing/Landing.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <section id="a11y">
+    <section id="a11y" class="project_item">
       <header class="container global_title--section-main">
         <h2 class="h1 global_subtitle">Accessibility Experience</h2>
       </header>
+      <article class="container">
+      <p>In my most recent position as a web developer at Tableau, I established an accessibility program for our
+        marketing website. I've been working with product teams looking to integrate end to end testing as well as
+        reporting progress regularly to executive stakeholders. I wrote about <a href="http://bit.ly/bringing-a11y" target="_blank">bringing accessibility to Tableau <span class="sr-only">opens a new window</span> </a> and how <a href="http://bit.ly/tableau-a11y" target="_blank">I used Tableau to
+          accomplish these goals <span class="sr-only">opens a new window</span></a>.</p>
+        <p>As an ongoing pursuit for transparency, <a href="https://github.com/lesliedoherty/portfolio/issues" target="blank">I created github issues for accessibility errors <span class="sr-only"> link opens in a new window</span></a> on this portfolio.
+          You can submit an issue or <a href="#contact">contact me <span class="sr-only">link scrolls to bottom</span> </a> with any concerns or trouble you have accessing information here.
+          I'm committed to quality and will continue to work towards WCAGAA compliance on every website I have the authority to.
+        </p>
+      </article>
     </section>
     <section id="projects">
     <header class="container global_title--section-main">
-      <h2 class="h1 global_subtitle">Recent Work</h2>
+      <h2 class="h1 global_subtitle">Projects</h2>
     </header>
     <project-items v-bind:projects="projects"></project-items>
     </section>
