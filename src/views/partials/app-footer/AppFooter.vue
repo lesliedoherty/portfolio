80647c095320e48fbe5cf8fc6bdc68073ddaa24b
Leslie Doherty <ldoherty@tableau.com>
2020-01-16 22:33:12 +0100

    Update to footer

--- src/views/partials/app-footer/AppFooter.vue
+++ src/views/partials/app-footer/AppFooter.vue
@@ -3,20 +3,19 @@
     <section class="container">
       <div class="footer_links">
         <a class="footer_links--item" href="https://github.com/lesliedoherty" target="_blank">
-          Github
+          Github <span class="sr-only">link takes you to new page.</span>
         </a>
         <a class="footer_links--item" href="https://twitter.com/mrsflinger" target="_blank">
-          Twitter
+          Twitter <span class="sr-only"> link takes you to a new page with the twitter profile for @mrsflinger.</span>
         </a>
         <a class="footer_links--item" href="https://instagram.com/mrsflinger" target="_blank">
-          Pretty(ish) Pictures
+          Pretty(ish) Pictures <span class="sr-only">on instagram. Link takes you to a new page.</span>
         </a>
       </div>
       <div class="footer_misc">
-        <h4> &copy;2017</h4>
+        <h4> &copy;2020</h4>
         <ul>
-          <li>Please don't use any of this content without permission, not even code.</li>
-          <li>Please do learn and ask how I can mentor you or work for you.</li>
+          <li>Please learn and ask how I can mentor you or work for you.</li>
           <li>Please do kindly tell me how I can improve my code.</li>
           <li>As always, this is a work in progress, just like I am.</li>
         </ul>
